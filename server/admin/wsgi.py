7e3b1c0c33210ac7347c1cc1f440ca598af8f444
1126680959@qq.com <1126680959@qq.com>
2025-01-15 15:46:37 +0800

    增加nltk_data目录

--- server/admin/wsgi.py
+++ server/admin/wsgi.py
@@ -8,14 +8,22 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import nltk
 
 from django.core.wsgi import get_wsgi_application
 from chat_app.initialize import initialize_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_server.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_server.settings")
 
 # 初始化服务
-#initialize_model()
+# initialize_model()
 
+# 添加nltk目录
+# 获取上级目录
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# 上级目录下的chat_app/libs/nltk_data目录
+nltk_data_dir = os.path.join(parent_dir, "chat_app/libs/nltk_data")
+
+nltk.data.path.append(nltk_data_dir)
 
 application = get_wsgi_application()
