a0f1c4593d9c4e6b8d39d4147a35547c9f08a393
1126680959@qq.com <1126680959@qq.com>
2025-01-17 14:14:21 +0800

    更新gpu解析

--- server/chat_app/knowledge/docment_loaders/ocr.py
+++ server/chat_app/knowledge/docment_loaders/ocr.py
@@ -1,10 +1,39 @@
+import logging
 from rapidocr_onnxruntime import RapidOCR
 
 
-def get_ocr() -> "RapidOCR":
-    """使用cpu"""
-    from rapidocr_onnxruntime import RapidOCR
+logger = logging.getLogger("chat_app")
 
-    ocr = RapidOCR()
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    try:
+        from rapidocr_paddle import RapidOCR
+    except ImportError:
+        from rapidocr_onnxruntime import RapidOCR
+
+
+def get_ocr(use_cuda: bool = True) -> "RapidOCR":
+    try:
+        from rapidocr_paddle import RapidOCR
+
+        ocr = RapidOCR(
+            det_use_cuda=use_cuda, cls_use_cuda=use_cuda, rec_use_cuda=use_cuda
+        )
+    except ImportError:
+        from rapidocr_onnxruntime import RapidOCR
+
+        ocr = RapidOCR()
     return ocr
+
+
+if __name__ == "__main__":
+    # ocr = get_ocr()
+    # image_path = "E:/idea_workspace/pythonProject/hx_gpt/img/1.png"
+    # result, elapse_list = ocr(image_path)
+    # print(result)
+    # print(elapse_list)
+    import paddle
+
+    print(paddle.utils.run_check())
